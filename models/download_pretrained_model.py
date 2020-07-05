a679ccc07fec5912381a7777802eb988384c9987
Lalatiana <mrakotosaon@gmail.com>
2020-07-05 19:12:54 +0200

    updated share link

--- models/download_pretrained_model.py
+++ models/download_pretrained_model.py
@@ -19,7 +19,7 @@ def download_dataset(source_url, target_dir, target_file):
 
 
 if __name__ == '__main__':
-    source_url = 'https://nuage.lix.polytechnique.fr/index.php/s/r6nmfsCQayrozD9/download'
+    source_url = 'https://nuage.lix.polytechnique.fr/index.php/s/yqWanZ8pNqftEPm/download'
     target_dir = os.path.dirname(os.path.abspath(__file__))
     target_file = os.path.join(target_dir, 'intrinsic_interpolation_pretrained_DFAUST.tar.gz')
     download_dataset(source_url,  target_dir, target_file)
